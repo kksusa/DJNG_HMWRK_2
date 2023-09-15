03ae5e2e9ad8f211c3317db392a4fb60dd32d891
kksusa <kksusa1@gmail.com>
2023-09-15 19:35:43 +0500

    Домашнее задание 5

--- homework_app/admin.py
+++ homework_app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import User, Product, Order
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number']
+    ordering = ['name']
+    list_filter = ['name']
+    search_help_text = 'Поиск пользователя по имени'
+    fields = ['name', 'email', 'phone_number', 'registration']
+    readonly_fields = ['name', 'registration']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'content', 'price']
+    ordering = ['name']
+    list_filter = ['name']
+    search_help_text = 'Поиск продукта по названию'
+    fields = ['name', 'content', 'price', 'count', 'image']
+    readonly_fields = ['name']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'sum_price', 'order_date']
+    ordering = ['-order_date']
+    list_filter = ['user_name']
+    search_help_text = 'Поиск заказа по пользователю'
+    fields = ['user_name', 'products', 'sum_price', 'order_date']
+    readonly_fields = ['order_date']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
